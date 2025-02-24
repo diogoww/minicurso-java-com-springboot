284dd94d4e6d088388941418e7ccf5bb4737457e
diogo varaschin <diogovaraschin@outlook.com>
2025-02-24 09:53:16 -0300

    incluindo o hash de senha na aplicação

--- todolist/src/main/java/br/com/diogovaraschin/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/diogovaraschin/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,6 +24,12 @@ public class UserController {
 			System.out.println("usuario ja existe");
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("usuário ja existe!");
 		}
+		
+		var passwordHashred = BCrypt.withDefaults()
+				.hashToString(12, userModel.getPassword().toCharArray());
+		
+		userModel.setPassword(passwordHashred);
+		
 		var userCreated = this.userRepository.save(userModel);
 		return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 	}
