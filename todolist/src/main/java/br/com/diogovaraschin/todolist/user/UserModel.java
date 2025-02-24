8d0742a81b5dd3b21f253e9fce8b2cca6f27518c
diogo varaschin <diogovaraschin@outlook.com>
2025-02-24 08:21:01 -0300

    criando tabela usuário

--- todolist/src/main/java/br/com/diogovaraschin/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/diogovaraschin/todolist/user/UserModel.java
@@ -1,9 +1,27 @@
 package br.com.diogovaraschin.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name="tb_users")
 public class UserModel {
+	
+	@Id
+	@GeneratedValue(generator="UUID")
+	private UUID id;
+	
 	private String username;
 	private String name;
 	private String password;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
 	
 	public String getUsername() {
 		return username;
